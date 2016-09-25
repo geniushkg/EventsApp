a5c508b894a3aad036f96f199e84dc9750cfcfbd
geniushkg <geniushkg@gmail.com>
2016-09-25 11:09:33 +0530

    chore : Initial commit setup

---
+++ app/src/main/java/com/hardikgoswami/eventsapp/Eventlist_act.java
@@ -0,0 +1,13 @@
+package com.hardikgoswami.eventsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Eventlist_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eventlist_act);
+    }
+}
