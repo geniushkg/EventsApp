3191c3ef21778642a0ac2693f5923a92584ec133
geniushkg <geniushkg@gmail.com>
2016-09-25 12:07:48 +0530

    feat : add view and presenter definition to contract
    add contract interfaces for concrete implementation

--- app/src/main/java/com/hardikgoswami/eventsapp/eventslist/EventListContract.java
+++ app/src/main/java/com/hardikgoswami/eventsapp/eventslist/EventListContract.java
@@ -1,7 +1,23 @@
 package com.hardikgoswami.eventsapp.eventslist;
 
+import com.hardikgoswami.eventsapp.BasePresenter;
+import com.hardikgoswami.eventsapp.BaseView;
+
 /**
  * Created by geniushkg on 9/25/2016.
  */
+/*
+* contract class for eventlist features it defines presenter and view interface
+* */
 public class EventListContract {
+
+    interface View extends BaseView<Presenter>{
+        void setLoadingIndication(boolean active);
+        void showEvents(List<Event> eventList);
+    }
+
+    interface Presenter extends BasePresenter{
+
+    }
+
 }
